47183e70b49fdf6a08e8e8554fb7321504806872
tanvirgeekios <tanvir.alam@iosventure.com>
2021-03-11 16:11:45 +0600

    Everything working

--- createpaymentintent.php
+++ createpaymentintent.php
@@ -5,7 +5,7 @@ try{
 
 require_once('./stripe-php/init.php');
 
-\Stripe\Stripe::setApiKey('sk_test_nPdYeoBLMmIE4lYcuX0ixLc700aonA2sCZ');
+\Stripe\Stripe::setApiKey('sk_test_51ISaH3BacYjVR52UAkAeTOMUq7OrpGaPWJkQufosBpywkbdzxDtf37wXT6qwEKAXL36H7uPeUVUi8JwdIMOEb0Nc00qtQZ23Ta');
 
 if (!isset($_POST['amount']) || !isset($_POST['currency']))
 {
